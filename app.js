533c9c7376b4b23bc943a1e384a1df0442c7aefa
Yasir <112541705+sirosiyasir@users.noreply.github.com>
2022-11-09 22:55:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu-title")
 const menuLinks = document.getElementById("menu-links")
+const mobileNavbar = document.getElementById("mobile-navbar")
+const iconMenu = document.getElementById("icon-menu")
 
 menu.onclick = () => {
   if (menuLinks.style.display === "none") {
@@ -8,3 +10,18 @@ menu.onclick = () => {
     menuLinks.style.display = "none"
   }
 }
+
+iconMenu.onclick = () => {
+  $("#mobile-navbar").slideToggle()
+  /* if (mobileNavbar.style.display === "none") {
+    mobileNavbar.style.display = "block"
+  } else {
+    mobileNavbar.style.display = "none"
+  } */
+}
+
+$(window).resize(function () {
+  if (window.innerWidth <= 575) {
+    mobileNavbar.style.display = "none"
+  }
+})
